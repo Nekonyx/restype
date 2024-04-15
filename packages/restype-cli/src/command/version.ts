07f75e5170ed13ad7ab588833f8c84e3aad1c1de
Nekonyx <nekonyx@proton.me>
2024-04-15 12:08:22 +0500

    fix(cli): extract `execute` and `filterPackages` methods because `handler` loses context

--- packages/restype-cli/src/command/version.ts
+++ packages/restype-cli/src/command/version.ts
@@ -1,16 +1,38 @@
-import { exec } from 'child_process'
-import { Arguments, CommandModule } from 'yargs'
+import { exec } from 'node:child_process'
+import { CommandModule } from 'yargs'
+
+function execute(command: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error)
+      }
+
+      resolve(stdout || stderr)
+    })
+  })
+}
+
+function filterPackages(input: string): string[] {
+  const matches = input.match(/\@restype\/(.*)\@([0-9\.]+)/g)
+
+  if (!matches) {
+    return []
+  }
+
+  return matches.map((match) => match.trim())
+}
 
 export class VersionCommand implements CommandModule {
   public readonly command = 'version'
   public readonly describe = 'Show version information'
 
-  public async handler(args: Arguments) {
-    const localOutput = await this.execute('npm list --depth=0')
-    const globalOutput = await this.execute('npm list -g --depth=0')
+  public async handler() {
+    const localOutput = await execute('npm list --depth=0')
+    const globalOutput = await execute('npm list -g --depth=0')
 
-    const local = this.filterPackages(localOutput)
-    const global = this.filterPackages(globalOutput)
+    const local = filterPackages(localOutput)
+    const global = filterPackages(globalOutput)
 
     console.log('Locally installed:')
     if (local.length === 0) {
@@ -25,27 +47,5 @@ export class VersionCommand implements CommandModule {
     } else {
       global.forEach((pkg) => console.log(`  ${pkg}`))
     }
-  }
-
-  private async execute(command: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          reject(error)
-        }
-
-        resolve(stdout || stderr)
-      })
-    })
-  }
-
-  private filterPackages(input: string): string[] {
-    const matches = input.match(/\@restype\/(.*)\@([0-9\.]+)/g)
-
-    if (!matches) {
-      return []
-    }
-
-    return matches.map((match) => match.trim())
   }
 }
